745a7807cf4a0e536c0e7d0241306060ec71a645
moriahchen34 <vidal.mariahchen@gmail.com>
2022-02-17 21:40:46 +0800

    first commit

---
+++ Activity 1/src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello, Mariah Chen</h1>
+<div>
+  Mariah the great and the wonderful. 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
