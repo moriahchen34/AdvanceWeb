3ac9dccfa192fa571e987d7f164ba141173d3561
moriahchen34 <vidal.mariahchen@gmail.com>
2022-03-19 21:48:13 +0800

    PrelimExam

---
+++ PrelimExam/script.js
@@ -0,0 +1,15 @@
+document.getElementById("Home").onclick = function() {
+    location.href = "#home";
+};
+
+document.getElementById("About").onclick = function() {
+    location.href = "#about";
+};
+
+document.getElementById("Education").onclick = function() {
+    location.href = "#education";
+};
+
+document.getElementById("Hobbies").onclick = function() {
+    location.href = "#hobbies";
+};
